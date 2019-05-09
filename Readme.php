a7e38db2d5549235517c5e47a7f1a0e498f6404b
Marlon Suarez <marlonsuarez@la-MacBook-Air-de-Marlon.local>
2019-05-08 23:20:44 -0500

    Create Readme.php

---
+++ Readme.php
@@ -0,0 +1,73 @@
+<!DOCTYPE html>
+<html>
+    <head>
+		<meta charset="UTF-8">
+		 <!-- +++++++++++ -->
+		<meta name="viewport" content="width=device-width, initial-scale=1">
+		 <!-- Colocamos la etiqueta title para dar titulo a la pagina -->
+		<title>Prácticas Colombia</title>
+		<!-- Agregar icono de la página-->
+		<link rel="shortcut icon" type="image/x-icon" href="Img/LogotipoPracticasColombia.ico">
+		<!--Bootstrap nav-->
+		<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+		<!--Enlace Estilos -->
+        <link rel="stylesheet" href="css/LoginCss.css">
+    </head>
+
+    <body>
+    	<!-- Barra de Navegación -->
+		<nav class="navbar navbar-expand-lg fixed-top ">
+			<a class="navbar-brand" href="#"> <img src="Img/LogotipoPracticasColombia.ico" width="50px" alt="">
+			</a>
+			<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+				<span class="navbar-toggler-icon"></span>
+			</button>
+			<div class="collapse navbar-collapse " id="navbarSupportedContent">
+				<ul class="navbar-nav mr-4">
+					<li class="nav-item">
+						<a class="nav-link" data-value="about" href="Index.html">Inicio</a> </li>
+					<li class="nav-item">
+						<a class="nav-link " data-value="portfolio" href="Readme.html">Acerca de</a>
+						</li>
+					<li class="nav-item">
+						<a class="nav-link " data-value="team" href="EquipoDeTrabajo.html">Equipo</a>
+						</li>
+					 <li class="nav-item">
+						<a class="nav-link " data-value="blog" href="License.html">Licencia</a>
+						</li>
+					<li class="nav-item">
+						<a class="nav-link " href="mailto:atencionalusuario@practicascolombia.com">Contacto</a>
+						</li>
+				</ul>
+			</div>
+		</nav>
+		<br>
+		<br>
+		<br>
+
+		<div class="container-fluid padding">
+			<div class="row welcome text-center">
+				<div class="col-12">
+					<h1 class="display-4">Acerca de</h1>
+					<div class="card">
+						<br>
+						<section>
+							<aside>
+								<img src="img/cambiarimagen4.jpg" class="img-fluid" alt="Cambiar Imagen 4" width="300px">
+							</aside>
+							<article>
+								<p>
+								Pagina web creada por estudiantes de ingenieria para solucionar el problema de las practicas en Colombia.
+								Constantemente estaremos asociando mas empresas para que los usuarios tengan mas oportunidad de completar su proceso de practicas<br/>
+								Si tienes alguna sugerencia o tienes idea de alguna empresa que este dispuesta a contar con nuestro servicio puedes ir a la seccion de contacto y escribirnos
+								</p>
+							</article>
+						</section>
+						<br>
+					</div>
+				</div>
+			</div>
+		</div>
+	</body>
+
+</html>
